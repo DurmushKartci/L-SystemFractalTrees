0362412a66998d4dc11dc77f16d7e9ec0a5ae7c3
DurmushKartci <durmusikartci@gmail.com>
2021-04-26 10:29:33 +0300

    Added turtle function for drawing trees with generations

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var axiom = "F";
 var sentence = axiom;
-
+let angle;
+var len = 100;
 var rule = [];
 
 rule[0] = {
@@ -25,16 +26,36 @@ function generate() {
     }
     sentence = nextSentence;
     createP(sentence);
+    turtle();
+}
+
+function turtle() {
+    len *= 0.55;
+    for (var i = 0; i < sentence.length; i++) {
+        var current = sentence.charAt(i);
+        if (current == "F") {
+            line(0, 0, 0, -len);
+            translate(0, -len)
+        } else if (current == "+") {
+            rotate(angle);
+        } else if (current == "-") {
+            rotate(-angle);
+        } else if (current == "[") {
+            push();
+        } else if (current == "]") {
+            pop();
+        }
+    }
 }
 
 function setup() {
-    noCanvas();
-    // createCanvas(500, 500);
+    angle = radians(25);
+    createCanvas(500, 500);
+    background(51);
+    translate(width / 2, height);
+    stroke(255, 100);
+    turtle();
     createP(axiom);
     let button = createButton("Generate");
     button.mousePressed(generate);
 }
-
-// function draw() {
-//     background(51);
-// }
